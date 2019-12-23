dc0e238927b3c912563c8eb7efe82a080fa04ce3
Nikita Shalin <nikita.shalin@studimobile.ru>
2019-12-23 10:45:41 +0300

    1: fix report

--- witcher.server/src/main/java/com/mpi/witcher/server/controllers/ReportController.java
+++ witcher.server/src/main/java/com/mpi/witcher/server/controllers/ReportController.java
@@ -118,7 +118,7 @@ public class ReportController {
         List<String> labels = new ArrayList<>();
         int month = calendar.get(Calendar.MONTH);
         while (month < endMonth) {
-            labels.add(MONTHS[month++]);
+            labels.add(MONTHS[1 + month++]);
         }
         List<ReportResponse.ChartData.Dataset> datasets = new ArrayList<>();
         for(User user : users) {
@@ -166,7 +166,7 @@ public class ReportController {
         List<String> labels = new ArrayList<>();
         int month = calendar.get(Calendar.MONTH);
         while (month < endMonth) {
-            labels.add(MONTHS[month++]);
+            labels.add(MONTHS[1 + month++]);
         }
         List<ReportResponse.ChartData.Dataset> datasets = new ArrayList<>();
         for(User user : users) {
@@ -214,7 +214,7 @@ public class ReportController {
         List<String> labels = new ArrayList<>();
         int month = calendar.get(Calendar.MONTH);
         while (month < endMonth) {
-            labels.add(Integer.toString(month++));
+            labels.add(MONTHS[1 + month++]);
         }
         List<ReportResponse.ChartData.Dataset> datasets = new ArrayList<>();
         for(User user : users) {
